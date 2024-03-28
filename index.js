0c16f18697e259c0093892c89110005c6eb79b89
Mohau Mushi <david1mushi1@gmail.com>
2024-03-28 11:27:41 +0200

    Called generatePlaylist and display the playlists for each Guardian

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const songs = [
     { title: "Fooled Around and Fell in Love", artist: "Elvin Bishop", genre: "Rock" },
     // Feel free to add even more songs
     { title: "Shape of You", artist: "Ed Sheeran", genre: "Pop"},
-    { title: "Uptown Funk", artist: "Mark Ronson ft. Bruno Mars", genre: "Funk"},
+    { title: "Another", artist: "Muni Long", genre: "R&B"},
     { title: "Hotel California", artist: "Eagles", genre: "Rock"},
     { title: "Bohemian Rhapsody", artist: "Queen", genre: "Rock"},
     { title: "Rolling in the Deep", artist: "Adele", genre: "Pop"},
@@ -40,6 +40,36 @@ function generatePlaylist(guardians, songs) {
 }
 
 // Call generatePlaylist and display the playlists for each Guardian
-generatePlaylist(guardians, songs);
+let playlists =  generatePlaylist(guardians, songs);
 
+    let playlistsDiv = document.getElementById('playlists');
 
+    playlists.forEach(playlist => {
+
+      const { guardian, songs } = playlist;
+  
+      const playlistElement = document.createElement('div');
+      playlistElement.classList.add('playlist');
+
+      let guardianHeader = document.createElement("h2");
+      guardianHeader.textContent = `${guardian}'s Playlist`;
+      playlistElement.appendChild(guardianHeader);
+
+      
+      songs.forEach(song => {
+        const songElement = document.createElement('div');
+        songElement.classList.add('song');
+
+        const songTitle = document.createElement('span');
+        songTitle.classList.add('song-title');
+        songTitle.textContent = song.title;
+        songElement.appendChild(songTitle);
+
+        const songArtist = document.createElement('span');
+        songArtist.textContent = ` by ${song.artist}`;
+        songElement.appendChild(songArtist);
+
+        playlistElement.appendChild(songElement);
+      });
+      playlistsDiv.appendChild(playlistElement);
+    });
